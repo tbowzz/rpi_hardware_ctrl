3c9d0aa0c452cc903f41f271fc918664a42f12b1
Tyler Bowers <tyler.j.bowers@gmail.com>
2019-01-15 00:21:10 +0000

    Enhanced esc.py

--- esc/esc.py
+++ esc/esc.py
@@ -7,32 +7,49 @@ pwm = Adafruit_PCA9685.PCA9685()
 frequency = 2000
 servo_min = 900
 servo_max = 1400
-gain_0 = 0
+gain_0 = -50
 gain_1 = 75
-gain_2 = 0
+gain_2 = -50
 gain_3 = 75
+enable0 = True
+enable1 = True
+enable2 = True
+enable3 = True
 
 # Helper function to make setting a servo pulse width simpler.
 def command_all_motors(val):
-    pwm.set_pwm(0, 0, val + gain_0)
-    pwm.set_pwm(1, 0, val + gain_1)
-    pwm.set_pwm(2, 0, val + gain_2)
-    pwm.set_pwm(3, 0, val + gain_3)
+    if enable0:
+        pwm.set_pwm(0, 0, val + gain_0)
+    if enable1:
+        pwm.set_pwm(1, 0, val + gain_1)
+    if enable2:
+        pwm.set_pwm(2, 0, val + gain_2)
+    if enable3:
+        pwm.set_pwm(3, 0, val + gain_3)
 
 def stop_all():
-    pwm.set_pwm(0, 0, servo_min)
-    pwm.set_pwm(1, 0, servo_min)
-    pwm.set_pwm(2, 0, servo_min)
-    pwm.set_pwm(3, 0, servo_min)
+    if enable0:
+        pwm.set_pwm(0, 0, servo_min)
+    if enable1:
+        pwm.set_pwm(1, 0, servo_min)
+    if enable2:
+        pwm.set_pwm(2, 0, servo_min)
+    if enable3:
+        pwm.set_pwm(3, 0, servo_min)
+    Adafruit_PCA9685.PCA9685()
 
 def setup():
     pwm.set_pwm_freq(frequency)
     time.sleep(1)
     print("Set frequency to ", frequency)
-    pwm.set_pwm(0, 0, servo_min + gain_0)
-    pwm.set_pwm(1, 0, servo_min + gain_1)
-    pwm.set_pwm(2, 0, servo_min + gain_2)
-    pwm.set_pwm(3, 0, servo_min + gain_3)
+    if enable0:
+        pwm.set_pwm(0, 0, servo_min + gain_0)
+    if enable1:
+        pwm.set_pwm(1, 0, servo_min + gain_1)
+    if enable2:
+        pwm.set_pwm(2, 0, servo_min + gain_2)
+    if enable3:
+        pwm.set_pwm(3, 0, servo_min + gain_3)
     time.sleep(2)
     print("Setup complete")
 
